cf4cd9c37df526a89c6247b4506f85239cb1421c
Raph6n <naim.raphael@outlook.fr>
2024-02-19 12:16:10 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-int main ()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
 {
-    //string initialisation
-    char Mystr[] = "Programming is like building a multilingual puzzle"
-    puts(Mystr); //writing the string to stdout
-
-    return 0;
+puts("`\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
