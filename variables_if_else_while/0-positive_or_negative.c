535acdd9a829927cbf80c7540e045f807d582adb
Raph6n <naim.raphael@outlook.fr>
2024-02-20 09:20:42 +0000

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%o is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n > 0)
 {
-printf("%o is negative\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
