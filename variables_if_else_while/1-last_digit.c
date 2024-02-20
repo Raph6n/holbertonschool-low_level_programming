cee15c16ae51dd7959b1e42e6b83b79251a7d5d8
Raph6n <naim.raphael@outlook.fr>
2024-02-20 10:20:39 +0000

    2

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,17 +10,5 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (last digit of n is greater than 5)
-	{
-	  printf("the string and is greater than 5");
-	}
-	else if (the last digit of n is 0)
-	{
-	  printf("the string and is 0");
-	}
-	else (if the last digit of n is less than 6 and not 0)
-	{
-	    printf("the string and is less than 6 and not 0");
-	}
-	return (0);
+ 
 }
