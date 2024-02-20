7c479bb2d3f7f15b123d02ebfc34cb5629e9e364
Raph6n <naim.raphael@outlook.fr>
2024-02-20 10:52:37 +0000

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar(letter);
+	grep (letter 'a' and 'q')
+	  return (0);
+}
