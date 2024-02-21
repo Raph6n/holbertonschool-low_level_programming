4559fac667ab942fa69dd05030fa24986372523e
Raph6n <naim.raphael@outlook.fr>
2024-02-21 14:53:31 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * prints the alphabet, in lowercase
- * void print_alphabet(void)
- *
+ * main - check the code
+ * 
+ *Return: Always 0.
  */
 
 void print_alphabet(void)
