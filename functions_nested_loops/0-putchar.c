cefd8aedd3102ab2ba7b7f484bb4a5891eb41ac1
Raph6n <naim.raphael@outlook.fr>
2024-02-21 10:13:42 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,13 +2,8 @@
 
 int main(void)
 {
- int putchar ("p");
- int putchar ("u");
-   int putchar ("t");
-   int putchar ("c");
-   int putchar ("h");
-   int putchar ("a");
-   int putchar ("r");
+ int putchar ('p');
+
 
     return (0);
 }
