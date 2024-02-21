3750fc9939fca16861d3d8465d775f49db4d6d8e
Raph6n <naim.raphael@outlook.fr>
2024-02-21 14:54:21 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,15 +4,15 @@
 /**
  * main - check the code
  * 
- *Return: Always 0.
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-	_putchar('\n');
+_putchar('\n');
 }
