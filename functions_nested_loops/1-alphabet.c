5a9a687e202958976f97adf4943e3c2ccb367fd9
Raph6n <naim.raphael@outlook.fr>
2024-02-21 14:23:58 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(void)
+/**
+ * void print_alphabet(void)
+ *
+ */
+
+void print_alphabet(void)
 {
   char print_alphabet = 'a';
+  
   while (print_alphabet <= 'z')
+    {
     putchar(alphabet);
   alphabet++;
-
+    }
   putchar('\n');
-  return (0);
 
 }
