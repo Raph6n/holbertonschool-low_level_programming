c77d1c1b81e849acc0825bebf4a1cacdc02fbe0d
Raph6n <naim.raphael@outlook.fr>
2024-02-21 08:15:50 +0000

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - base 10 with putchar
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+int base;
+
+for (base = 0; base < 10; base++)	  
+putchar(base % 10) + '0');
+return (0);
+}
+
