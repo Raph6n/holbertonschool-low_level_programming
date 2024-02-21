608d7e50928bba653148324bf868832a76a67d83
Raph6n <naim.raphael@outlook.fr>
2024-02-21 08:21:26 +0000

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,11 @@ int main(void)
 {
 int base;
 
-for (base = 0; base < 10; base++)	  
-putchar(base % 10) + '0');
+for (base = 0; base < 10; base++)
+  putchar((base % 10) + '0');
+
+putchar('\n');
+
 return (0);
 }
 
