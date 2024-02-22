f5d618d5fe62c8943796cf6d05b53c3eb72bc350
Raph6n <naim.raphael@outlook.fr>
2024-02-22 13:04:17 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isalpha(int c);
+
+{
+  char letter;
+
+  for (letter 'a'; letter <= 'z'; letter++)
+    _putchar ('0')
+      if (letter 'c');
+  {
+    putchar ('1')
+      }
+  
