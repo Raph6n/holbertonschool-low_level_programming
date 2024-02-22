9bc6dcdf91f1041ffc94b8b76b2ccfa0676f533e
Raph6n <naim.raphael@outlook.fr>
2024-02-22 12:22:05 +0000

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - print alphabet x10
+ * Description: printing alphabet x10 with putchar
+ *
+ * Return: void
+ */
+
+void  print_alphabet_x10(void)
+{
+int count = 0;
+char letter;
+
+while (count++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
+} 
+
