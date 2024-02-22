ac200b1e23c8b43d0c6a9c3b290845e60b157ec1
Raph6n <naim.raphael@outlook.fr>
2024-02-22 14:17:08 +0000

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * main - check letter c lowercase
+ * Return: 1 or 0
+ * @c: variable
+ */
 int _isalpha(int c)
 
 {
-  if ( c >= 'a' && <= 'z')
-    {
-      return (1);
-    }
-  return (0);
+if ((c >= 'a') && (c <= 'z'))
+{
+return (1);
+}
+return (0);
 }
