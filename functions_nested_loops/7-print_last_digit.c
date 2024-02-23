34f00149956c841c39188588f594dc17f50d281d
Raph6n <naim.raphael@outlook.fr>
2024-02-23 14:46:40 +0000

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,18 @@
 int print_last_digit(int lst)
 
 {
-  if (lst >=  0)
-    {
-      lst = lst % 10;
-  _putchar('0' + lst);
+if (lst >=  0)
+{
+lst = lst % 10;
+_putchar('0' + lst);
 }
-  else if (lst < 0)
-    {
-      lst = lst % 10 * (-1);
+else if (lst < 0)
+{
+lst = lst % 10 * (-1);
    
-   _putchar('0' + lst);
-   }
-  return lst;
-    }
+_putchar('0' + lst);
+}
+return (lst);
+}
  
   
