af1854dcf986ed6fc5374427d4faa04e92eaebb3
Raph6n <naim.raphael@outlook.fr>
2024-02-23 14:25:11 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code
+ * @n: variable 98
+ * Return: Always 0.
+ */
+
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
 
-	else
-	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
-	}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
 }
