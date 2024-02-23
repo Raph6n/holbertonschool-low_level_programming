ef6dcc696b7738760ba542c8f078a635bf0cdc94
Raph6n <naim.raphael@outlook.fr>
2024-02-23 10:55:47 +0000

    jb

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,8 +3,7 @@
 int print_last_digit(int lst)
 
 {
-  if (lst < 0 && lst > 0)
-    _putchar (lst % 10);
-      return lst;
+  if (lst <= 0 && lst= > 0)
+   
 }
   
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+  for ( number >= 0; number <=0; number++);
+for (number >= 0; number <=0; number++);
+_putchar (':');
+	   for (number >=0; number <=0; number ++);
+for (number >=0; number <=0; number ++);
+return 0
+  }
