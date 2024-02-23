6b1f7faf3d887a49af78cc7a310d083e0020e3d6
Raph6n <naim.raphael@outlook.fr>
2024-02-23 08:34:30 +0000

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-int _abs(int)
+int _abs(int);
 
 {
-  if (_abs < '0') && (_abs > '0'))
+  if ( _abs < '0') && ( _abs > '0'))
   {
-    _putchar(%d\n);
+    _putchar (%d);
   }
 return 0
 }
