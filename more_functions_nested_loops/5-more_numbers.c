f0b572b1c776f851f2a641f13406aa4981adfc3b
Raph6n <naim.raphael@outlook.fr>
2024-02-26 12:26:28 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,9 @@ void more_numbers(void)
   int b = 0;
   char a;
   
-  while (b++ <= "14")
+  while (b++ <= 14)
     {
-  for (a = '0'; a <= "14"; a++)
+  for (a = 0; a <= 14; a++)
     _putchar (a);
   _putchar('\n');
     }
