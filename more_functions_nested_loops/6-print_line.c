c20e1e0e61de8cc1a8e91677917e19e1f54de209
Raph6n <naim.raphael@outlook.fr>
2024-02-26 15:09:22 +0000

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,4 +3,12 @@
 void print_line(int n)
 
 {
-  
+  for (n = '0'; n < '0'; n++)
+
+    _putchar ('\n')
+      if (n = '0')
+      else if (n = '2')
+      else if (n = '10')
+      else if (n = '-4')
+	_putchar ('_')
+	  }
