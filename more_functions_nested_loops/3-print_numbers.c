82c5214aaaeb3cf643e690ecae7a0655e9dbab48
Raph6n <naim.raphael@outlook.fr>
2024-02-26 10:36:30 +0000

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,9 +5,9 @@ void print_numbers(void)
 {
   int a;
   
-  for ((a >= '0') && (a <= '9'))
+  for (a >= '0'; a <= '9'; a++)
     {
-      _putchar (%d);
+      _putchar (a);
     }
   return (0);
 }
