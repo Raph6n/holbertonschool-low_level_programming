a47818221e4b2bdd4b567b60dfcf86e71a85ee84
Raph6n <naim.raphael@outlook.fr>
2024-02-26 18:19:42 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,4 +8,19 @@ void print_diagonal(int n)
       _putchar ('\n')
       else
 	{
-	  
+	  int a, b;
+
+	  for (a = 0; a < n; a++)
+	    {
+	      for (b = 0; b < n; b++)
+		{
+		  if (a == b)
+		    _putchar ('\\');
+		  else (a > b)
+			 _putchar(' ');
+		}
+	      _putchar('\n')
+		}
+	}
+    }
+			      
