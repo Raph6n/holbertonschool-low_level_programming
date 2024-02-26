fcb39cf6405b86f5807b7140a3565f4fc1189233
Raph6n <naim.raphael@outlook.fr>
2024-02-26 09:33:24 +0000

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+
 int _isupper(int c)
 
 {
-  if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 
-    {
-      return (1);
-	}
-  return (0);
 }
