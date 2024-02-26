0a0c1dfe590b9aa6c1ef0c65fa1172ed875915ab
Raph6n <naim.raphael@outlook.fr>
2024-02-26 11:52:45 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,9 @@ void print_most_numbers(void)
   char a;
 
   for (a = '0'; a <= '9'; a++)
-    {
-    _putchar(a);
-    }
+    {   
   if ((a != '2') || (a != '4'))
-    {
       _putchar (a);
     }
+  _putchar ('\n');
 }
