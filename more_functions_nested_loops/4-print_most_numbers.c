3584f3ec8da8776a98ab9eeb5e5b0f24dbc917a0
Raph6n <naim.raphael@outlook.fr>
2024-02-26 11:36:06 +0000

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_most_numbers
+
+{
+  char a;
+
+  for (a > '0'; a <'9'; a++)
+    {
+    _putchar (a);
+    }
+  if ((a = 2) && (a = 4))
+    {
+      return (0);
+    }
+}
