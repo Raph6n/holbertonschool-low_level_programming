e23d8370eeae745052a6e578f515b80a2ff409d2
Raph6n <naim.raphael@outlook.fr>
2024-02-26 09:52:50 +0000

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _isdigit - explain
+ * 
+ * return: 1 or 0.
+ *
+ * @c: explain
+ */
+
 int _isdigit(int c)
 
 {
-  if ((c >= '0') && (c <= '9'))
+if ((c >= '0') && (c <= '9'))
 
-    return (1);
-    else
-      return (0);
-	}
+return (1);
+else
+return (0);
+}
