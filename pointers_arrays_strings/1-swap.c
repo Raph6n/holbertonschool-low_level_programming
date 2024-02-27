bdedd45cd34c93068fd457e8585696e079677c7c
Raph6n <naim.raphael@outlook.fr>
2024-02-27 12:58:58 +0000

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -5,25 +5,8 @@ void swap_int(int *a, int *b)
 
 {
   int c;
-  *a = *b;
+
+  c = *a;
+  *a = b*;
   *b = c;
-}
-
-int main()
-{
-  int d, e;
-
-  printf("Enter value of d: ");
-  scanf("%d" ,&d);
-  printf("Enter value of e: ");
-  scanf("%d" ,&e);
-
-  printf("Befor swapping: d is:%d, e is: %d\n" ,d,e);
-
-  swap_int(&d ,&e);
-
-  printf("After swapping: d is: %d, e is: %d\" ,d,e);
-
-  return 0;
-
 }
