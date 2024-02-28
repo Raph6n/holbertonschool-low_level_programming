9e7b592f0952ac9231292b1fbbcf650c5887ed00
Raph6n <naim.raphael@outlook.fr>
2024-02-28 09:13:28 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_rev - reverse string
+ * Return: 0
+ * @s: variable pointeur s
  */
 
 void print_rev(char *s)
 
+{
+if (*s)
+{
+print_rev(s + 1);
+_putchar(*s);
+}
+}
   
