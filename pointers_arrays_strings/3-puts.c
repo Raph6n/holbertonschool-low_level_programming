376def04b321db9f6ee2abde1b08386245f6a8e0
Raph6n <naim.raphael@outlook.fr>
2024-02-28 08:50:40 +0000

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * _puts - print a string stdout
+ * Return: 0
+ * @str: variable
+ */
+
 void _puts(char *str)
 
 {
 
-  while (*str != '\0')
-    {
-    _putchar(*str++);
-      }
+while (*str != '\0')
+{
+_putchar(*str++);
+}
 _putchar('\n');
 }
