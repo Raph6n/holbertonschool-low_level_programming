70187185dd855416f1d5ea9e3a1ce5674188e4a9
Raph6n <naim.raphael@outlook.fr>
2024-02-28 14:21:02 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * puts2 - print 1 caracter on 2
+ * Return: 0
+ * @str: variable
+ */
+
 void puts2(char *str)
 
 {
@@ -10,7 +16,7 @@ while (str[a] != '\0')
 {
 _putchar(str[a]);
 a += 2;
- }
+}
 _putchar('\n');
 }
 
