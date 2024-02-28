be7094a53997e58b57beefcdb16b92d974a0b803
Raph6n <naim.raphael@outlook.fr>
2024-02-28 08:44:58 +0000

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,12 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * swap_int - swap 2 int value
+ * Return: 0
+ * @a: variable
+ * @b: variable
+ */
+
 void swap_int(int *a, int *b)
 
 {
-  int c;
+int c;
 
-  c = *a;
-  *a = *b;
-  *b = c;
+c = *a;
+*a = *b;
+*b = c;
 }
