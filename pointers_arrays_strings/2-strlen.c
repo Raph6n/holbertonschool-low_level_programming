6b6741f64b4d693dbb23eb164f63610ab37a62a6
Raph6n <naim.raphael@outlook.fr>
2024-02-28 08:48:37 +0000

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strlen - retourner taille string
+ * Return: compter
+ * @s: variable s
+ */
+
 int _strlen(char *s)
 
 {
-    int compter = 0;
+int compter = 0;
 while (*s != '\0')
-  {
-compter++;
-s++;
-    }
-    return compter;
+{
+(compter++);
+(s++);
+}
+return (compter);
 }
