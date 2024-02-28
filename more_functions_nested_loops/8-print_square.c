f4cc4ed2b4d2be824334ff9329fe5da0c8655325
Raph6n <naim.raphael@outlook.fr>
2024-02-28 08:27:09 +0000

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,29 @@
 #include "main.h"
 
+/**
+ * print_square - print a square
+ * Return: void
+ * @size: variable
+ */
+
 void print_square(int size)
 
 {
- if (size <= 0)
-    {
-    _putchar ('\n');
-    }
- else
-   {
-     int a, b;
+if (size <= 0)
+{
+_putchar ('\n');
+}
+else
+{
+int a, b;
 
-     for (a = 0; a < size; a++)
-       {
-	 for (b = 0; b < size; b++)
-	   {
-	     _putchar('#');
-	   }
-	 _putchar('\n');
-	   }
-   }
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
