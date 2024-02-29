aa86b0f3995f50320b9d5b2d48d20c8d074ec083
Raph6n <naim.raphael@outlook.fr>
2024-02-29 17:40:31 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,2 +1,28 @@
 #include "main.h"
 
+int main()
+
+{
+int a;
+
+for(a = 1;a <= 100;a++)
+{
+  if (a % 3 == 0 && a % 5 == 0)
+    {
+      _putchar("FizzBuzz");
+    }
+  else if (a % 3 == 0)
+    {
+      _putchar("Fizz");
+    }
+  else if (a % 5 == 0)
+    {
+      _putchar("Buzz");
+    }
+  else
+    {
+      _putchar(a);
+    }
+ }
+}
+      
