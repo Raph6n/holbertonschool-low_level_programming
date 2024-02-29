27268df22a2ee72d2c25a330fb884a13560c3af1
Raph6n <naim.raphael@outlook.fr>
2024-02-29 18:07:30 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,16 +5,18 @@ char *_strcat(char *dest, char *src)
 
 {
 
-  char *a = dest;
-
-  while (*a != '\0')
-    {
-    a++;
-    }
-  while (*src != '\0')
-    {
-      *a=*src;
-      *a++;
-      *src++
-	}
-  
+char stock = dest;
+ while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return stock;
+}
+ 
