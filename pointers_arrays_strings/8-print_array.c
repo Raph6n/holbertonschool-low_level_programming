071df8a680232aa3a3082afd6fef7189599adf1f
Raph6n <naim.raphael@outlook.fr>
2024-02-29 08:54:42 +0000

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - print n elements of an array
+ * Return: 0
+ * @n: variable
+ * @a: variable
+ */
+
+
 void print_array(int *a, int n)
 
 {
@@ -16,6 +24,3 @@ printf(", %d", a[b]);
 }
 printf("\n");
 }
-
-  
-  
