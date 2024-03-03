b9d34dd76973122f18b5296aef0840a1cca793c8
Raph6n <naim.raphael@outlook.fr>
2024-03-03 11:07:48 +0000

    22

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,9 @@
 /**
  * _strncpy - copier la string
  * Return: char copie
+ * @dest: var col
+ * @src: var source
+ * @n: compter
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -11,14 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for(i = 0; i < n && src[i] != '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
-for( ; i < n; i++)
+for ( ; i < n; i++)
 {
 dest[i] = '\0';
 }
-return dest;
+return (dest);
 }
-  
