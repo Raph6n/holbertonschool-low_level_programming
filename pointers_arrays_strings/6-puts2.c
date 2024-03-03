ec74b6dc9fe815b9d1fc805ea96caa36f330d212
Raph6n <naim.raphael@outlook.fr>
2024-03-03 19:10:32 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,13 +3,11 @@
 /**
  * puts2 - print 1 caracter on 2
  * Return: 0
- * @str: variable, on intitie & a 0 donc premiere lettre du tableru, tant que le caractere du tableau nest pas nul, on affiche une lettre sur 2
+ * @str: variable, on intitie & alettre sur 2
  */
 
 void puts2(char *str)
-
 {
-
 int a = 0;
 
 while (str[a] != '\0')
@@ -19,4 +17,3 @@ a += 2;
 }
 _putchar('\n');
 }
-
