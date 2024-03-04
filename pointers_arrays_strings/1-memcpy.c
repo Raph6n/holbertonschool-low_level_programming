36e028a8bc52c3ea738d76c17d926a06e4c67624
Raph6n <naim.raphael@outlook.fr>
2024-03-04 09:48:53 +0000

    111

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _memcpy - fonction
+ * @dest: var char 1
+ * @ src: var char 2
+ * @n: var int 1
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-  unsigned int i;
-
-
-
-
-  return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
