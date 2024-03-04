7f6a9e88ac978a166723d13f87539cc9fbba3ace
Raph6n <naim.raphael@outlook.fr>
2024-03-04 15:13:37 +0000

    777

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_chessboard - fonction principale
+ * return: 0
+ * @a: var
+ */
+
 void print_chessboard(char (*a)[8])
 
 {
 
-  int w;
-  int b;
+int w;
+int b;
 
-  for (w = 0; w < 8; w++)
-   {
-  for (b = 0; b < 8; b++)
-    {
-      printf("%c", a[w][b]);
-    }
-      printf("\n");
-   }
+for (w = 0; w < 8; w++)
+{
+for (b = 0; b < 8; b++)
+{
+printf("%c", a[w][b]);
+}
+printf("\n");
+}
 }
