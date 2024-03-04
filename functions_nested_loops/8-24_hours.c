9b5377131d6cd9f3658c114c8c716939a806e132
Raph6n <naim.raphael@outlook.fr>
2024-03-04 17:32:27 +0000

    jb

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,10 +2,16 @@
 
 void jack_bauer(void)
 {
-  for ( number >= 0; number <=0; number++);
-for (number >= 0; number <=0; number++);
-_putchar (':');
-	   for (number >=0; number <=0; number ++);
-for (number >=0; number <=0; number ++);
-return 0
-  }
+  int a;
+
+  for (a = 0; a <= 9; a++)
+{
+for (int b = 0; b <=  9; b++)
+{
+_putchar ('0' + a);
+_putchar ('0' + b);
+_putchar ('\n');
+}
+
+}
+}
