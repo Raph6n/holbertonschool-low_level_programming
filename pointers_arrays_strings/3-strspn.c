561037bc8a5cb67b311c2e6cd203ddf6696041b5
Raph6n <naim.raphael@outlook.fr>
2024-03-05 10:46:12 +0000

    333

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,21 +5,20 @@ unsigned int _strspn(char *s, char *accept)
 
 {
 
-int i = 0;
+  int i = 0;
+  int b;
 
-while (*s != 0)
-{
-char *c = accept;
- while (*c != 0)
-   {
-     if (*s == *c)
-       {
-	 i++;
-	 break;
-       }
-     c++;
-   }
- if (*c == 0)break;
- accept++;
- }
+  while (s[i] != '\0')
+
+    {
+      b = 0;
+
+      while (accept[b] != '\0' && s[i] != accept[b])
+	b++;
+      if (accept[b] == '\0')
+	return (i);
+      i++;
+    }
+  return (i);
+}
 
