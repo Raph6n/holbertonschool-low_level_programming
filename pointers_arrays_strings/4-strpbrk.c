ea45c4df3ef766ed86767b2d4207c29711d3d2da
Raph6n <naim.raphael@outlook.fr>
2024-03-05 13:32:27 +0000

    444

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - fonction
+ * return: 0
+ *@s: var char 1
+ *@accept: var char 2
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+int i;
+
+while (*s != '\0')
+{
+for (i = 0; accept[i] != '\0'; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return (0);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -43,3 +43,5 @@ char *_strchr(char *s, char c);
  unsigned int _strspn(char *s, char *accept);
 
  void print_chessboard(char (*a)[8]);
+
+char *_strpbrk(char *s, char *accept);
