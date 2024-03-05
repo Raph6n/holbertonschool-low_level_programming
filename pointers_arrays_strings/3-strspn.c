31d847aebda7eaf1bd395ce1f198533254bcac21
Raph6n <naim.raphael@outlook.fr>
2024-03-05 10:01:20 +0000

    333

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,6 @@ unsigned int _strspn(char *s, char *accept)
 int i = 0;
 
 while (*s != 0)
-
 {
 char *c = accept;
  while (*c != 0)
@@ -23,4 +22,4 @@ char *c = accept;
  if (*c == 0)break;
  accept++;
  }
-}
+
