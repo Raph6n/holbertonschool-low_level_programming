3e6706402c0e0e91f0fc2fa5c694144ae3e1cd12
Raph6n <naim.raphael@outlook.fr>
2024-03-05 09:58:17 +0000

    333

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,3 +4,23 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
+
+int i = 0;
+
+while (*s != 0)
+
+{
+char *c = accept;
+ while (*c != 0)
+   {
+     if (*s == *c)
+       {
+	 i++;
+	 break;
+       }
+     c++;
+   }
+ if (*c == 0)break;
+ a++;
+ }
+}
