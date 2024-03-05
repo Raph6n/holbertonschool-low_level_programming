9193425ed61176b68cc472ad7f1cb9f2a71525b9
Raph6n <naim.raphael@outlook.fr>
2024-03-05 13:26:14 +0000

    333

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _strspn - fonction
+ * return: i
+ * @s: var char 1
+ *@accept: var char 2
+ */
+
 unsigned int _strspn(char *s, char *accept)
 
 {
 
-  int i = 0;
-  int ii;
+int i = 0;
+int ii;
 
-  while (s[i] != '\0')
-    {
-      ii = 0;
-      while (accept[ii] != '\0' && s[i] != accept[ii])
-	  ii++;
-      if (accept[ii] == '\0')
-	return i;
-      i++;
-    }
-  return (i);
+while (s[i] != '\0')
+{
+ii = 0;
+while (accept[ii] != '\0' && s[i] != accept[ii])
+ii++;
+if (accept[ii] == '\0')
+return (i);
+i++;
+}
+return (i);
 }
