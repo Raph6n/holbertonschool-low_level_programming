5de9f93cb139032d507d6a2cc2490bc2498b6cac
Raph6n <naim.raphael@outlook.fr>
2024-03-06 13:52:42 +0000

    3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * factorial - fction
+ * return: depend
+ * @n: variable
+ */
+
 int factorial(int n)
 {
-  
+if (n < 0)
+return -1;
+else if ( n == 0)
+return 1;
+else
+return n * factorial(n - 1);
+}
