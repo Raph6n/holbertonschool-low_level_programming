ff01c6adb8d91b4d0711b4130e8211d3b87b0583
Raph6n <naim.raphael@outlook.fr>
2024-03-06 12:43:17 +0000

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _puts_recursion - recursion fonction
+ * return: void
+ *
+ * @s:var 
+ */
+
+
 void _puts_recursion(char *s)
 
 {
 
-  if (*s == '\0')
-    {
-      _putchar ('\n');
-      return;
-    }
-  _putchar(*s);
-  _puts_recursion(s + 1);
+if (*s == '\0')
+{
+_putchar ('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
