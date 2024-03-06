10da48e25b7d5b66230044fb294c0f442edbcbbb
Raph6n <naim.raphael@outlook.fr>
2024-03-06 13:18:40 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _strlen_recursion - fonction rec
+ * return: 0
+ * @s: var char
+ */
+
+
 int _strlen_recursion(char *s)
 
 {
+
+if (*s == '\0')
+return (0);
+else
+return ( 1 + _strlen_recursion(s + 1));
+}
