27aa0a839bbf283c5f42f828fcc2c024a0cece79
Raph6n <naim.raphael@outlook.fr>
2024-03-07 08:57:40 +0000

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - fction main
+ * @argc: var 1
+ * @argv: var 2
+ * return: 0
+ */
+
 int main(int argc, char *argv[])
 
 {
-  
+
+if (argc < 0)
+{
+printf("%c", agrv[])
+}
+return (0);
+}
