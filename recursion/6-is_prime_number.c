91d165ef817b482928ca08c52383b309d969f0a9
Raph6n <naim.raphael@outlook.fr>
2024-03-08 16:49:26 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,22 +4,14 @@
 int is_prime_number(int n)
 
 {
-if (n <= 1)
-{
-return (0);
-}
-if (n <= 3)
-{
-return (1);
-}
+  if (n <= 1) return 0;
+  if (n <= 3) return 1;
 
 int i;
 
  for (i = 2; i * i <= n; i++)
    {
-     if (n % i == 0)
-       {
-	 return (0);
-       }
+     if (n % i == 0) return 0;
+   }
      return (1);
    }
