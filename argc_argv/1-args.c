cf0b9292e53da66e57cd315e13d8ee5101ab09d8
Raph6n <naim.raphael@outlook.fr>
2024-03-08 11:41:00 +0000

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - fonction
+ * Return: 0
+ * @argc: var int
+ * @argv: var char
+ */
+
+
+int main(argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+
+return(0);
+}
