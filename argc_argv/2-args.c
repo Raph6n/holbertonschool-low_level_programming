6c7fd4f689035104d7b8ee6ddd20d6d139b13533
Raph6n <naim.raphael@outlook.fr>
2024-03-08 16:39:06 +0000

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - fction
+ * @argc: var
+ * @argv: var
+ * Return: 0
+ */
 
 int main(int argc,char *argv[])
 
 {
-  int i;
+int i;
 
-  for(i = 0; i < argc; i++)
-    {
-      printf("%s\n", argv[i]);
-    }
-  return(0);
+for(i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return(0);
 }
