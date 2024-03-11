f03af1bbf5f13a5712813364e8a8699d200aa70d
Raph6n <naim.raphael@outlook.fr>
2024-03-11 13:21:12 +0000

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,4 +5,22 @@
 char *create_array(unsigned int size, char c);
 
 {
+  int i*;
+  char *a;
   
+if (size == 0)
+{
+return (NULL);
+}
+ a = malloc(size *sizeof(char));
+
+ if (a == NULL)
+   {
+     return (NULL);
+   }
+ for (i = 0; i < size; i++)
+   {
+     a[i] = c
+       }
+ return (a);
+}
