3fe73b2144a3ff4d251db62a9f2bad14369bd163
Raph6n <naim.raphael@outlook.fr>
2024-03-11 14:20:41 +0000

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,6 +2,37 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strdup - fction
+ * @str: var
+ * Return: b
+ */
+
 char *_strdup(char *str)
 
 {
+int a = 0;
+int i = 1;
+char *b;
+
+if (str == NULL)
+{
+return (NULL)
+}
+ while (str[i])
+{
+i++;
+}
+b = malloc((sizeof(char) * i)+1);
+if (b == NULL)
+{
+return (NULL);
+}
+while (a < i)
+{
+b[a] = str[a];
+a++;
+}
+b[a] = '\0';
+return (b);
+}
