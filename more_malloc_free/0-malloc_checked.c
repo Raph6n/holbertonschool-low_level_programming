f0133ac7b41bfd1fd633ce5deb3372d31d6132af
Raph6n <naim.raphael@outlook.fr>
2024-03-12 13:00:11 +0000

    0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * malloc_checked - fction
+ * @b: int var
+ * Return: p
+ */
+
 void *malloc_checked(unsigned int b);
+{
+int *p;
+p = malloc(b);
+if (p == NULL)
+{
+exit (98);
+}
+return (p);
+}
