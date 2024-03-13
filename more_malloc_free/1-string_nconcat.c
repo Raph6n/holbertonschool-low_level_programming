acd890410a0a1ddb17dd5953cfb668bc1b4682e0
Raph6n <naim.raphael@outlook.fr>
2024-03-13 09:10:40 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,20 +13,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-int compte_1 = 0, compte_2 = 0, i, nouvelle_taille;
+unsigned int compte_1 = 0, compte_2 = 0, i, nouvelle_taille;
 char *nouvelle;
 
 while (s1 != NULL && s1[compte_1] != '\0')
 {
 compte_1++;
 }
-while (s2 != NULL && s2[compte_2] != '\0' && compteur_2 < n)
+while (s2 != NULL && s2[compte_2] != '\0' && compte_2 < n)
 {
-compteur_2++;
+compte_2++;
 }
 nouvelle_taille = compte_1 + compte_2 + 1;
  
-nouvelle =(char *)malloc(nouvelle_taille * (sizeof(char));
+nouvelle =(char *)malloc(nouvelle_taille * sizeof(char));
 if (nouvelle == NULL)
 {
 return (NULL);
@@ -37,6 +37,8 @@ nouvelle[i] = s1[i];
 }
 for (i = 0; i < compte_2; i++)
 {
-nouvelle[compte_1 + compte_2 = '\0'];
+nouvelle[compte_1 + i] = s2[i];
+}
+nouvelle[compte_1 + compte_2] = '\0';
 return (nouvelle);
 }
