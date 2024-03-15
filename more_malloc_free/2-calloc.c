d80ef711dc7edea0c50189d59961c58faa95eb01
Raph6n <naim.raphael@outlook.fr>
2024-03-15 08:41:45 +0000

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -5,7 +5,22 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
+unsigned int i;
+int *p;
+
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
+}
+
+p = malloc(size * nmemb);
+
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < memb; i++)
+{
+p[i] = 0;
+return (p);
 }
