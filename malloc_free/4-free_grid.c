8fb17033ce710c06db31ced828eeb626af4a2907
Raph6n <naim.raphael@outlook.fr>
2024-03-15 19:04:04 +0000

    4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,6 +2,23 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - fction
+ * @grid: var
+ * @height: var
+ * Return: --
+ */
+
 void free_grid(int **grid, int height)
 {
+int i;
   
+if (grid != NULL && height > 0)
+{
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
+}
