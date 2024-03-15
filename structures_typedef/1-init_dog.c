937b45560e4ab9faafc4c5acc5402d38017db0fd
Raph6n <naim.raphael@outlook.fr>
2024-03-15 08:10:49 +0000

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+/**
+ * init_dog - fction
+ * @d: var
+ * @name: var
+ * @age: var
+ * @owner: var
+ */
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-  
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
