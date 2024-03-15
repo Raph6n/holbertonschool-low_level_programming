360064095dc93ea63d68c32fd212b45b23b6a309
Raph6n <naim.raphael@outlook.fr>
2024-03-15 10:53:09 +0000

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * array_range - fction
+ * @min: var
+ * @max: var
+ * Return: p
+ */
+
 int *array_range(int min, int max);
 
 {
-  
+int *p;
+int ecart, i;
+if (min > max)
+{
+return (NULL);
+}
+ecart = max - min + 1;
+p = malloc(ecart * sizeof(*p));
+
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ecart; min++, i++)
+{
+p[i] = min;
+return (p);
+}
+}
+   
