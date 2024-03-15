aabee6d45e70c376fdd317ba729ead004c26a4ab
Raph6n <naim.raphael@outlook.fr>
2024-03-15 15:15:19 +0000

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,8 +1,35 @@
-#include <stdio.>
+#include <stdio.h>
 #include "dog.h"
 
-void print_dog(struct dog *d);
+void print_dog(struct dog *d)
 
 {
-  if (d == NULL)
-    
+if (d == NULL)
+{
+return;
+}
+if (d->name == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("name: %s\n", d->name);
+}
+if (d->age < 0)
+{
+printf("(nil)");
+}
+else
+{
+printf("age: %f\n", d->age);
+}
+if (d->owner == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("owner: %s\n", d->owner);
+}
+}
