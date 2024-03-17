55e4d40736da78eeda9d64f0bd3d3e44e368bd05
Raph6n <naim.raphael@outlook.fr>
2024-03-17 08:23:27 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - fction
+ * Return: 0
+ */
+
 int main(void)
 
 {
 int number1, number2;
- 
+
 for (number1 = 0; number1 <= 8; number1++)
 {
 for (number2 = number1 + 1; number2 <= 9; number2++)
@@ -14,11 +19,11 @@ putchar('0' + number2);
 
 if (!(number1 == 8 && number2 == 9))
 {
-putchar(','); 
+putchar(',');
 putchar(' ');
 }
 }
 }
-return 0;
+return (0);
 }
 
