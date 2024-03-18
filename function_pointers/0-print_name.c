53ad397a12060851d910f4e0928bf327b2a8123d
Raph6n <naim.raphael@outlook.fr>
2024-03-18 09:00:03 +0000

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *))
+/**
+ * print_name - fction
+ * @name: var
+ * @f: var
+ */
+
+print_name(char *name, void (*f)(char *))
 
 {
-  if (name != NULL && f != NULL)
-
+if (name != NULL && f != NULL)
 {
 printf("My name is %s\n", name);
 }
