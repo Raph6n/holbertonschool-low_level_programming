afcfdc16ce26778c1d558caa0d04295a3e84517c
Raph6n <naim.raphael@outlook.fr>
2024-03-19 14:34:56 +0000

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * sum_them_all - fction var
+ * @n: var
+ * Return: sum
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
+int sum;
   
+if (n == 0)
+{
+return (0);
+}
+va_list args;
+va_start(args,n);
+
+for (int i = 0; i < n; i++)
+{
+sum += va_args(args, int);
+}
+va_end(args);
+return (sum);
+}
+      
