ca56baf85ebb09cb0aeba439d4af531b2b6fce9b
Raph6n <naim.raphael@outlook.fr>
2024-03-20 09:54:16 +0000

    prepa 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 
 {
   
