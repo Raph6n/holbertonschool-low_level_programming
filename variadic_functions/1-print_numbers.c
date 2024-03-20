a11434c4b47f73ae7b40f963f468f856f0ce8d5e
Raph6n <naim.raphael@outlook.fr>
2024-03-20 15:33:19 +0000

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - fction
+ * @separator: var
+ * @n: var
+ * Return: void
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 
 {
-  
+
+va_list others;
+unsigned int i;
+
+va_start(others, n);
+
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(others, int));
+
+if (separator != NULL && i < (n - 1))
+{
+printf("%s", separator);
+}
+}
+va_end(others);
+printf("\n");
+}
