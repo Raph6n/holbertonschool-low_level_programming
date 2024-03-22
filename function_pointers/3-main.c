6619cb77738478291c54dd762aceff385fd0e5b2
Raph6n <naim.raphael@outlook.fr>
2024-03-22 17:17:39 +0000

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 int main (int argc, char *argv[])
 {
 int nu1, nu2, result;
 int (*op)(int, int);
+char *str;
 
 if (argc != 4)
 {
-  printf("Error\n");
+printf("Error\n");
 return (98);
 }
-op = get_op_func(argv[2]);
+nu1 = atoi(argv[1]);
+nu2 = atoi(argv[3]);
+str = argv[2];
 
-if (op == NULL)
-{
-printf("Error\n");
-return (99);
+ if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+   {
+     printf("Error\n");
+     return (99);
+   }
+ op = get_op_func(str);
+ result = op(nu1, nu2);
+
+ printf("%d\n", result);
+ return (0);
 }
- nu1 = atoi(argv[1]);
-   nu2 = atoi(argv[3]);
-   result = op(a, b);
-   printf("%d\n", result);
-   return (0);
-}
+ 
+ 
+
