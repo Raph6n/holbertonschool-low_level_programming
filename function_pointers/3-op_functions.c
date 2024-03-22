7187885a578249b37312b5b82d2e93c23df54024
Raph6n <naim.raphael@outlook.fr>
2024-03-22 21:59:34 +0000

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -14,14 +14,38 @@ int op_add(int a, int b)
 {
 return (a + b);
 }
+
+/**
+ * op_sub - fction less
+ * @a: var
+ * @b: var
+ * Return: result
+ */
+
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+
+/**
+ * op_mul - fction
+ * @a: var
+ * @b: var
+ * Return: result
+ */
+
+
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+/**
+ * op_div - fction
+ * @a: var
+ * @b: var
+ * Return: result
+ */
+
 int op_div(int a, int b)
 {
 if (b == 0)
@@ -31,6 +55,13 @@ exit(100);
 }
 return (a / b);
 }
+/**
+ * op_mod - fction
+ * @a: var
+ * @b: var
+ * Return: result
+ */
+
 int op_mod(int a, int b)
 {
 if (b == 0)
