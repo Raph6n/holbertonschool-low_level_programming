f91a295a63cef18b5405732655b41d0d9aaaca43
Raph6n <naim.raphael@outlook.fr>
2024-03-22 21:49:35 +0000

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int main (int argc, char *argv[])
+/**
+ * main - fction
+ * @argc: arg
+ * @argv: chaine
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
 int nu1, nu2;
 int (*op)(int, int);
@@ -15,17 +22,17 @@ exit(98);
 if (argv[2][1])
 {
 printf("Error\n");
-return (99);
+exit(99);
 }
- op = get_op_func(argv[2]);
- if (op == NULL)
-   {
-     printf("Error\n");
-     exit (100);
-   }
- nu1 = atoi(argv[1]);
- nu2 = atoi(argv[3]);
- printf("%d\n", op(nu1, nu2));
- return (0);
+op = get_op_func(argv[2]);
+if (op == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+nu1 = atoi(argv[1]);
+nu2 = atoi(argv[3]);
+printf("%d\n", op(nu1, nu2));
+return (0);
 }
 
