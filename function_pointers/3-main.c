66c587fa6b586b3125e874079961f93337bd53f6
Raph6n <naim.raphael@outlook.fr>
2024-03-22 11:46:52 +0000

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,25 +3,24 @@
 
 int main (int argc, char *argv[])
 {
-  int nu1, nu2, result;
-  int (*op_func)(int, int);
+int nu1, nu2, result;
+int (*op)(int, int);
 
-  if (argc != 4)
-    {
-      printf("Error\n");
-      return (98);
-    }
-  nu1 = atoi(argv[1]);
-  nu2 = atoi(argv[3]);
+if (argc != 4)
+{
+  printf("Error\n");
+return (98);
+}
+op = get_op_func(argv[2]);
 
-  op_func = get_op_func(argv[2]);
-
-  if (!op_func)
-    {
-      printf("Error\n");
-      return (99);
-    }
-  result = op_func(nu1, nu2);
-  printf("%d\n", result);
-  return (0);
+if (op == NULL)
+{
+printf("Error\n");
+return (99);
+}
+ nu1 = atoi(argv[1]);
+   nu2 = atoi(argv[3]);
+   result = op(a, b);
+   printf("%d\n", result);
+   return (0);
 }
